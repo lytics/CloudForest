e5a4d38d0470636c9eb693228f002d564e22a08b
J <j.e.aten@gmail.com>
2018-11-26 20:35:34 -0600

    cleanup comments

--- voter.go
+++ voter.go
@@ -3,14 +3,15 @@ package CloudForest
 // VoteTallyer  is used to tabulate votes by
 // trees and is implemented by feature type specific
 // structs like NumBallotBox and CatBallotBox.
-// Vote should register a cote that casei should be predicted as pred.
-// TallyError returns the error vs the supplied feature.
 type VoteTallyer interface {
+
+	// Vote should register a vote that casei should be predicted as pred.
 	Vote(casei int, pred string, weight float64)
+
+	// TallyError returns the error vs the supplied feature.
 	TallyError(feature Feature) float64
 
 	// Tally returns the predicted category string for casei.
-	// Regressors should just return an empty string.
 	Tally(casei int) string
 
 	// TallyNum returns the predicted regression for case i.
